0995deabd1523f08e7a00a7541865157f402ac25
NorwegianGoat <riccardomioli@gmail.com>
2022-01-15 13:37:51 +0100

    data indexing

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+from calendar import day_abbr
 import pandas
+import os
 
-if __name__=="__main__":
-    pass
+__DATA_PATH = './ml-25m'
+
+
+def load_data(path: str) -> pandas.DataFrame:
+    files = [os.path.join(__DATA_PATH, file) for file in os.listdir(path)]
+    files.remove('./ml-25m/README.txt')
+    print('Scanning', __DATA_PATH, "found:", files)
+    return [pandas.read_csv(file) for file in files]
+
+
+if __name__ == "__main__":
+    dfs = load_data(__DATA_PATH)
