dbcef74642886baa9db69f0228aec27f92ec321e
NorwegianGoat <riccardomioli@gmail.com>
2022-01-17 14:52:47 +0100

    Added genoma

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import io
 import pandas as pd
 import os
+import logging
 
 __DATA_PATH = './ml-25m'
+__logging_level = logging.DEBUG
 
 
 def load_data(path: str) -> pd.DataFrame:
     movies = pd.read_csv(os.path.join(
         __DATA_PATH, "movies.csv"), index_col="movieId", usecols=["movieId", "genres"])
     ratings = pd.read_csv(os.path.join(__DATA_PATH, "ratings.csv"))
+    genome = pd.read_csv(os.path.join(__DATA_PATH, "genome-scores.csv"))
     # Calc. avg. rating foreach movie
     y = ratings.groupby("movieId")["rating"].mean()
     movies = movies.merge(y, on="movieId")
@@ -16,6 +20,9 @@ def load_data(path: str) -> pd.DataFrame:
     movies = movies.merge(genres, on="movieId")
     # Remove old genres column
     movies.drop("genres", axis=1, inplace=True)
+    # Adding genome
+    genome = genome.pivot(index="movieId", columns="tagId", values="relevance")
+    movies = movies.merge(genome, on="movieId")
     return movies
 
 
@@ -27,8 +34,11 @@ def bin_rating(ratings: pd.Series) -> pd.Series:
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=__logging_level)
     df = load_data(__DATA_PATH)
-    # df.info(show_counts=True)
-    # print(df.head())
     # Binning avg rating
     df["rating"] = bin_rating(df["rating"])
+    df_info = io.StringIO()
+    df.info(show_counts=True, buf=df_info)
+    logging.info(df_info.getvalue())
+    logging.debug(df)
