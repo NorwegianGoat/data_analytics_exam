9fa21a16a6c014c09770b083cad4cb2f835d2252
NorwegianGoat <riccardomioli@gmail.com>
2022-01-17 09:49:45 +0100

    mean calc

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-from calendar import day_abbr
-import pandas
+import pandas as pd
 import os
 
 __DATA_PATH = './ml-25m'
+__genres = ["Action", "Adventure", "Animation", "Children", "Comedy",
+            "Crime", "Documentary", "Drama", "Fantasy", "Film-Noir", "Horror",
+            "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War",
+            "Western", "(no genres listed)"]
+
+def parse_genres(genres: pd.Series) -> pd.DataFrame:
+    genres = pd.get_dummies(genres.str.split("|", expand=True))
+    print(genres)
 
 
-def load_data(path: str) -> pandas.DataFrame:
-    files = [os.path.join(__DATA_PATH, file) for file in os.listdir(path)]
-    files.remove('./ml-25m/README.txt')
-    print('Scanning', __DATA_PATH, "found:", files)
-    return [pandas.read_csv(file) for file in files]
+def load_data(path: str) -> pd.DataFrame:
+    movies = pd.read_csv(os.path.join(__DATA_PATH, "movies.csv"), index_col="movieId")
+    ratings = pd.read_csv(os.path.join(__DATA_PATH, "ratings.csv"))
+    y = ratings.groupby("movieId")["rating"].mean()
+    movies = pd.merge(movies, y, on="movieId")
+    #df.info(show_counts=True)
+    '''df = df.reindex(axis=1, labels=df.columns.tolist() + __genres)
+    for i, genres in df["genres"].items():
+        genres = genres.split("|")
+        for genre in genres:
+            df.iloc[i][genre] = 1
+    print(df.head())'''
+    return df
 
 
 if __name__ == "__main__":
-    dfs = load_data(__DATA_PATH)
+    df = load_data(__DATA_PATH)
